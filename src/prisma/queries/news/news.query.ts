187e9327fc4addb1738f3a56d1830938bf310b14
fikiaprian23 <fikiaprian23@gmail.com>
2024-06-08 21:29:09 +0700

    add query news

--- src/prisma/queries/news/news.query.ts
+++ src/prisma/queries/news/news.query.ts
@@ -1,10 +1,67 @@
 import { Injectable } from '@nestjs/common';
 import { DbService } from '../../db.service';
-import { RegisterUserDto } from '../../../auth/dto/register-user.dto';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 
 @Injectable()
 export class NewsQuery extends DbService {
 
+    async create(payload: Prisma.NewsCreateInput, prismaTx?: PrismaClient) {
+        const prisma = prismaTx || this.prisma;
+        return await prisma.news.create({
+            data: payload
+        })
+    }
+
+    async update(id: string, payload: Prisma.NewsUpdateInput, prismaTx?: PrismaClient) {
+        const prisma = prismaTx || this.prisma;
+        return await prisma.news.update({
+            where: {
+                id
+            },
+            data: payload
+        })
+    }
+
+    async delete(id: string, prismaTx?: PrismaClient) {
+        const prisma = prismaTx || this.prisma;
+        return await prisma.news.delete({
+            where: {
+                id
+            }
+        })
+    }
+
+    async findAll() {
+        return await this.prisma.news.findMany();
+    }
+
+    async findById(id: string) {
+        return await this.prisma.news.findUnique({
+            where: {
+                id
+            }
+        })
+    }
+
+    async findByRangeDate({ startDate, endDate }: { startDate: Date, endDate: Date }) {
+        return await this.prisma.news.findMany({
+            where: {
+                createdAt: {
+                    gte: startDate,
+                    lte: endDate
+                }
+            }
+        })
+    }
+
+    async findByCurrentDay() {
+        return await this.prisma.news.findMany({
+            where: {
+                createdAt: {
+                    gte: new Date(new Date().setHours(0, 0, 0, 0))
+                }
+            }
+        })
+    }
 }
